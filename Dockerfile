fc2ca6719c6bd7f8a460e86da3e1dff3e6cbb288
roc <roc.liu@hotmail.com>
2024-10-21 22:12:43 +0800

    通过proxy-go 转发代理

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,15 @@ FROM ubuntu:20.04
 ENV DEBIAN_FRONTEND=noninteractive
 # 设置环境变量
 ENV UPSTREAM_PROXY=
-ENV FORWARD_DNS="223.5.5.5"
+ENV FORWARD_DNS="8.8.8.8"
 
 # 安装必要的软件
 RUN apt-get update && apt-get install -y \
-    dnsmasq \
-    iptables
+    dnsmasq
 # 安装 glider
 COPY glider/glider /usr/local/bin/glider
+COPY proxy-go/* /usr/local/bin/proxy-go/
+RUN chmod +x /usr/local/bin/proxy-go/proxy
 RUN mkdir /etc/dns2proxy/
 # 复制dnsmasq配置文件
 COPY dnsmasq.conf /etc/dns2proxy/dnsmasq.conf
@@ -25,7 +26,7 @@ COPY start.sh /start.sh
 RUN chmod +x /start.sh
 
 # 暴露DNS端口和代理端口
-EXPOSE 53/udp 53/tcp 8445
+EXPOSE 53/udp 53/tcp
 
 # 设置容器启动命令
 CMD ["/start.sh"]
--- start.sh
+++ start.sh
@@ -10,10 +10,10 @@ sed -i "s/CONTAINER_IP/$CONTAINER_IP/g" /etc/dns2proxy/dnsmasq.conf
 sed -i "s|UPSTREAM_PROXY|$UPSTREAM_PROXY|g" /etc/dns2proxy/glider.conf
 # 启动glider
 glider -config /etc/dns2proxy/glider.conf &
-
+/usr/local/bin/proxy-go/proxy  sps -P $UPSTREAM_PROXY  -p ":443,:80" -q 8.8.8.8:53`;
 # 设置iptables规则
-iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 8445
-iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 8445
+# iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 8445
+# iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 8445
 
 # 启动dnsmasq
 dnsmasq -C /etc/dns2proxy/dnsmasq.conf --no-daemon
